bfd2629b29fc3dca97ec80bec0696cd8a837f105
Dayana <dayana.toledo@unesp.br>
2021-03-12 14:40:59 -0300

    feat: understanding about components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { Button } from "./components/Button";
+
+import './styles/global.css'
+
 function App() {
   return (
-    <h1>Hello NLW4</h1>
+    <div>
+      <Button color="red">
+        Botão 1
+      </Button>
+      <Button color="blue">
+        Botão 2
+      </Button>
+      <Button color="yellow">
+        Botão 3
+      </Button>
+    </div>
   );
 }
 
