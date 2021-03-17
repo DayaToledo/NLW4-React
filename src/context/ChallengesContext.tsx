ef5fd98bc2555a0bc5470bc48aa09efcd2b85797
Dayana <dayana.toledo@unesp.br>
2021-03-16 22:24:07 -0300

    feat: add function startNewChallenge

--- src/context/ChallengesContext.tsx
+++ src/context/ChallengesContext.tsx
@@ -1,18 +1,48 @@
 import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json'
 
-export const ChallengesContext = createContext({});
-
+interface Challenge {
+    type: 'body' | 'eye';
+    description: String;
+    amount: number;
+}
+interface ChallengesContextData {
+    level: number; 
+    currentExperience: number; 
+    challengesCompleted: number;
+    activeChallenge: Challenge;
+    startNewChallenge: () => void;
+}
 interface ChallengesProviderProps {
     children: ReactNode;
 }
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
 
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [level, setLevel] = useState(1);
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+    const [activeChallenge, setActiveChallenge] = useState(null);
+
+    function startNewChallenge() {
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengeIndex];
+
+        setActiveChallenge(challenge);
+    }
+
     return (
-        <ChallengesContext.Provider value={{ level, currentExperience, challengesCompleted }}>
+        <ChallengesContext.Provider 
+            value={{ 
+                level, 
+                currentExperience, 
+                challengesCompleted,
+                activeChallenge, 
+                startNewChallenge
+            }}
+        >
             {children}
         </ChallengesContext.Provider>
     )
