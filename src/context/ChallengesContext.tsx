0c20e8a955b89f89c2ca785641d81199d089a7c7
Dayana <dayana.toledo@unesp.br>
2021-03-18 18:10:26 -0300

    feat: add Storage Cookies

--- src/context/ChallengesContext.tsx
+++ src/context/ChallengesContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useState, ReactNode, useEffect } from 'react';
+import Cookies from 'js-cookie';
 import challenges from '../../challenges.json'
 
 interface Challenge {
@@ -19,14 +20,17 @@ interface ChallengesContextData {
 }
 interface ChallengesProviderProps {
     children: ReactNode;
+    level: number,
+    currentExperience: number,
+    challengeCompleted: number
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
 
-export function ChallengesProvider({ children }: ChallengesProviderProps) {
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
-    const [challengesCompleted, setChallengesCompleted] = useState(0);
+export function ChallengesProvider({ children, ...rest }: ChallengesProviderProps) {
+    const [level, setLevel] = useState(rest.level ?? 1);
+    const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
+    const [challengesCompleted, setChallengesCompleted] = useState(rest.challengeCompleted ?? 0);
 
     const [activeChallenge, setActiveChallenge] = useState(null);
 
@@ -35,6 +39,12 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     useEffect(() => {
         Notification.requestPermission();
     }, [])
+
+    useEffect(() => {
+        Cookies.set('level', String(level));
+        Cookies.set('currentExperience', String(currentExperience));
+        Cookies.set('challengesCompleted', String(challengesCompleted));
+    }, [level, currentExperience, challengesCompleted])
 
     function levelUp() {
         setLevel(level + 1);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,62 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
+
 import { CountdownProvider } from '../context/CountdownContext';
+import { ChallengesProvider } from '../context/ChallengesContext';
 
 import styles from '../styles/pages/Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move it</title>
-      </Head>
+interface HomeProps {
+    level: number,
+    currentExperience: number,
+    challengeCompleted: number
+}
 
-      <ExperienceBar/>
-      
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile/>
-            <CompletedChallenges/>
-            <Countdown/>
-          </div>
-          <div>
-            <ChallengeBox/>
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+export default function Home(props) {
+  return (
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience} 
+      challengeCompleted={props.challengeCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move it</title>
+        </Head>
+
+        <ExperienceBar/>
+        
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile/>
+              <CompletedChallenges/>
+              <Countdown/>
+            </div>
+            <div>
+              <ChallengeBox/>
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengeCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengeCompleted: Number(challengeCompleted)
+    }
+  }
 }
